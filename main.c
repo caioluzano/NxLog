163a7a98dec232b561b2e2b8c43e3eb1fbf9425b
Caio Luzano <caioluzano@hotmail.com>
2016-03-08 22:13:54 -0300

    Primeira licao

--- main.c
+++ main.c
@@ -3,7 +3,13 @@
 
 int main(int argc, char *argv[])
 {
-  printf("Oi, mundo cruel\n");
-  exit(0);
+	int var1, var2;
+
+	scanf ("%d", &var1);
+	scanf ("%d", &var2);
+
+	printf("< %d + %d = %d\n" , var1, var2, var1+var2);
+	exit (0);
 }
+
 
